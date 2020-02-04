8582e2a606def27571711a66c726ec3582610a1c
Peretto Lucas <56082086+PerettoLucas@users.noreply.github.com>
2020-02-04 16:34:16 +0100

    Added Files

--- SocketProgrammingC/Client.c
+++ SocketProgrammingC/Client.c
@@ -4,16 +4,70 @@
 #include <sys/types.h>
 #include <netdb.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
-int main(int argc , char *argv[])
+int main(int argc, char const *argv[])
 {
-    //Socket erstellen
-    int sock = socket(domain, type, protocol);
+  //Socket erstellen
+  int clientsock = socket(AF_INET, SOCK_STREAM, 0);
 
-    //
-    int value = 1;
-    setsockopt(sock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &value, sizeof(value));
+  //zum Vermeiden von "address already in use":
+  int value = 1;
+  setsockopt(clientsock, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &value, sizeof(value));
+
+  //optional: an Port binden
+  struct sockaddr_in addr;
+  addr.sin_family = AF_INET;
+  addr.sin_addr.s_addr = INADDR_ANY;
+  addr.sin_port = htons(12345); //network-byte-order = big-endian
 
 
-    return 0;
+  bind(clientsock, &addr, sizeof(addr));
+
+  //TCP: connect
+  struct sockaddr_in serv_addr;
+  memset(&serv_addr, '0', sizeof(serv_addr));
+  serv_addr.sin_port = htons(7);
+  serv_addr.sin_family = AF_INET;
+  //TODO add argv adress possibility
+  if(inet_pton(AF_INET, "10.216.20.52", &serv_addr.sin_addr) > 0)
+  {
+    int c = connect(clientsock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+
+    if(c != 0)
+    {
+      printf("Connection could not be established\n");
+      exit(1);
+    }
+  }else
+  {
+    printf("Connection could not be established\n");
+    exit(1);
+  }
+
+  const int FLAGS = 0;
+  char buff[1024] = { 0 };
+
+
+  //doing the echo action
+  while (1)
+  {
+
+
+    fgets(buff,sizeof(buff),stdin);
+    //send buffer
+    send(clientsock, buff, strlen(buff), FLAGS);
+
+    //receive
+    int bytesread = recv(clientsock, buff, sizeof(buff), FLAGS);
+    if(bytesread <= 0)
+      return bytesread; //socket was closed
+
+    //Printing result
+    printf("%s", buff);
+
+  }
+
+  return 0;
 }
