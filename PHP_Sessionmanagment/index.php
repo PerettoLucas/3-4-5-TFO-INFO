326da8c2d7f36b03a3d316aab718923b4512c420
39388 <39388@192.168.1.100>
2020-05-10 21:25:45 +0200

    TODO continue work

--- PHP_Sessionmanagment/index.php
+++ PHP_Sessionmanagment/index.php
@@ -7,7 +7,16 @@
 <body>
 <a href="index.php?id=1">Benutzerliste</a>
 <a href="index.php?id=2">Neuer Benutzer</a>
-
+ 
+ <?php 
+ if(!isset($_GET["id"]) || $_GET["id"] == "1"){
+     require_once '';
+ }
+ 
+ 
+ 
+ 
+ ?>
 
 
 </body>
